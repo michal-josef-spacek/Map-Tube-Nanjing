31b0325bb2509963be43bd9b105301b8632b6add
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-01-18 16:55:36 +0100

    Remove simple comments

--- Nanjing.pm
+++ Nanjing.pm
@@ -1,16 +1,13 @@
 package Map::Tube::Nanjing;
 
-# Pragmas.
 use strict;
 use warnings;
 use 5.006;
 
-# Modules.
 use File::Share ':all';
 use Moo;
 use namespace::clean;
 
-# Version.
 our $VERSION = 0.05;
 
 # Get XML.
@@ -117,11 +114,9 @@ For more information about Nanjing Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE1
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(decode_utf8 encode_utf8);
  use Map::Tube::Nanjing;
 
@@ -139,11 +134,9 @@ For more information about Nanjing Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE2
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Map::Tube::Nanjing;
 
  # Object.
@@ -160,11 +153,9 @@ For more information about Nanjing Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE3
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Map::Tube::GraphViz;
  use Map::Tube::GraphViz::Utils qw(node_color_without_label);
  use Map::Tube::Nanjing;
@@ -198,11 +189,9 @@ For more information about Nanjing Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE4
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(encode_utf8);
  use Map::Tube::Nanjing;
 
@@ -225,11 +214,9 @@ For more information about Nanjing Map, click L<here|https://en.wikipedia.org/wi
 
 =head1 EXAMPLE5
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(decode_utf8 encode_utf8);
  use Map::Tube::Nanjing;
 
